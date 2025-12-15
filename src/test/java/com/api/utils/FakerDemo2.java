88a5c885496c1af4b4c07a3ba339f66ef7525497
tulinag <debika0989@gmail.com>
2025-12-15 23:19:04 +0000

    Practiced creating fake create job payload

---
+++ src/test/java/com/api/utils/FakerDemo2.java
@@ -0,0 +1,79 @@
+package com.api.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+import com.api.request.model.CreateJobPayload;
+import com.api.request.model.Customer;
+import com.api.request.model.CustomerAddress;
+import com.api.request.model.CustomerProduct;
+import com.api.request.model.Problems;
+import com.github.javafaker.Faker;
+
+public class FakerDemo2 {
+
+	private static final String COUNTRY = "India";
+
+	public static void main(String[] args) {
+
+		// I want to create fake customer
+
+		Faker faker = new Faker(new Locale("en-IND"));// India specific data
+
+		String fname = faker.name().firstName();
+		String lname = faker.name().lastName();
+		String mobileNumber = faker.numerify(("70########"));
+		String alternateMobileNumber = faker.numerify(("70########"));
+		String customerEmailAddress = faker.internet().emailAddress();
+
+		Customer customer = new Customer(fname, lname, mobileNumber, alternateMobileNumber, customerEmailAddress, null);
+		System.out.println(customer);
+
+		// customer address fake object creation
+		String flatNumber = faker.numerify(("####"));
+		String apartmentName = faker.address().streetName();
+		String streetName = faker.address().streetName();
+		String landmark = faker.address().streetName();
+		String area = faker.address().streetName();
+		String pinCode = faker.numerify(("#####"));
+
+		String state = faker.address().state();
+
+		CustomerAddress customerAddress = new CustomerAddress(flatNumber, apartmentName, streetName, landmark, area,
+				pinCode, COUNTRY, state);
+
+		System.out.println(customerAddress);
+
+		// customer product fake object creation
+
+		String dop = DateTimeUtil.getTimeWithDaysAgo(10);
+		String imeiSerialNumber = faker.numerify(("###############"));
+		String popUrl = faker.internet().url();
+
+		CustomerProduct customerProduct = new CustomerProduct(dop, imeiSerialNumber, imeiSerialNumber, imeiSerialNumber,
+				popUrl, 1, 1);
+
+		System.out.println(customerProduct);
+
+		// Problems
+
+		// want to generate random number between 1-27
+		Random random = new Random();
+		int problemId = random.nextInt(27) + 1;
+
+		String fakeRemark = faker.lorem().sentence(10);
+		Problems problems = new Problems(problemId, fakeRemark);
+		System.out.println(problems);
+
+		List<Problems> problemList = new ArrayList<Problems>();
+		problemList.add(problems);
+
+		CreateJobPayload payload = new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct,
+				problemList);
+		System.out.println(payload);
+
+	}
+
+}
