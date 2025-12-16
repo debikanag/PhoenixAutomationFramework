6097b212c6a19bee641117c4fca2eaea2b6274f7
tulinag <debika0989@gmail.com>
2025-12-16 12:56:59 +0000

    Created the faker data generator utility and also added the CreateJobAPIFakeDataDrivenTest into data driven package

---
+++ src/test/java/com/api/tests/CreateJobAPIwithFakeData.java
@@ -0,0 +1,46 @@
+package com.api.tests;
+
+import static com.api.utils.SpecUtil.requestSpecWithAuth;
+import static com.api.utils.SpecUtil.responseSpec_OK;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.startsWith;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.api.constant.Role;
+import com.api.request.model.CreateJobPayload;
+import com.api.utils.FakerDataGenerator;
+
+public class CreateJobAPIwithFakeData {
+
+	private Iterator<CreateJobPayload> createJobPayload;
+	private static final String COUNTRY = "India";
+
+	@BeforeMethod(description = "Create the request payload for create job api")
+	public void setup() {
+		
+
+		createJobPayload = FakerDataGenerator.generateFakeCreateJobData(10);
+	}
+
+	@Test(description = "Verify if create job api is able to create Inwarranty job", groups = { "api", "regression",
+			"smoke" })
+
+	public void CreateJobAPITest() throws IOException {
+
+		given().spec(requestSpecWithAuth(Role.FD, createJobPayload))
+
+				.when().post("/job/create").then().spec(responseSpec_OK())
+				.body(matchesJsonSchemaInClasspath("response-schema/CreateJobAPIResponseSchema.json"))
+				.body("message", equalTo("Job created successfully. ")).body("data.mst_service_location_id", equalTo(1))
+				.body("data.job_number", startsWith("JOB_"));
+
+	}
+
+}
--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -9,6 +9,7 @@ import org.testng.annotations.DataProvider;
 import com.api.request.model.CreateJobPayload;
 import com.api.utils.CSVReaderUtil;
 import com.api.utils.CreateJobBeanMapper;
+import com.api.utils.FakerDataGenerator;
 import com.dataproviders.api.bean.CreateJobBean;
 import com.dataproviders.api.bean.UserBean;
 
@@ -25,22 +26,29 @@ public class DataProviderUtils {
 
 	@DataProvider(name = "CreateJobAPIDataProvider", parallel = true)
 	public static Iterator<CreateJobPayload> CreateJobAPIDataProvider() {
-		
 
 		Iterator<CreateJobBean> createJobBeanIterator = CSVReaderUtil.loadCSV("testData/CreateJobData.csv",
 				CreateJobBean.class);
-		
+
 		List<CreateJobPayload> payloadList = new ArrayList<CreateJobPayload>();
 		CreateJobBean tempBean;
 		CreateJobPayload tempPayload;
-		while(createJobBeanIterator.hasNext())
-		{
-			tempBean= createJobBeanIterator.next();
-			tempPayload= CreateJobBeanMapper.mapper(tempBean);
+		while (createJobBeanIterator.hasNext()) {
+			tempBean = createJobBeanIterator.next();
+			tempPayload = CreateJobBeanMapper.mapper(tempBean);
 			payloadList.add(tempPayload);
 		}
-		
+
 		return payloadList.iterator();
 
 	}
+
+	@DataProvider(name = "CreateJobAPIFakerDataProvider", parallel = true)
+	public static Iterator<CreateJobPayload> CreateJobAPIFakerDataProvider() {
+
+		Iterator<CreateJobPayload> payLoadIterator = FakerDataGenerator.generateFakeCreateJobData(100);
+		return payLoadIterator;
+
+	}
+
 }
