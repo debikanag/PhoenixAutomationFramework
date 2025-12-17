718fb1277367559334068f308829fc20d7c73bd7
tulinag <debika0989@gmail.com>
2025-12-17 20:41:28 +0000

    Created the JSON Reader utility for login API test data

---
+++ src/test/java/com/api/tests/datadriven/LoginAPIJSONDataDrivenTest.java
@@ -0,0 +1,28 @@
+package com.api.tests.datadriven;
+
+import static com.api.utils.SpecUtil.requestSpec;
+import static com.api.utils.SpecUtil.responseSpec_OK;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import com.api.request.model.UserCredentials;
+
+public class LoginAPIJSONDataDrivenTest {
+
+	@Test(description = "Verifying if login api is working for FD user", groups = { "api", "regression",
+			"datadriven" ,"json"}, dataProviderClass = com.dataproviders.DataProviderUtils.class, dataProvider = "LoginAPIJsonDataProvider")
+
+	public void loginAPITest(UserCredentials userCredentials) throws IOException {
+
+		given().spec(requestSpec(userCredentials)).when().post("login").then().spec(responseSpec_OK())
+				.body("message", equalTo("Success")).and()
+				.body(matchesJsonSchemaInClasspath("response-schema/LoginResponseSchema.json"));
+
+	}
+
+}
