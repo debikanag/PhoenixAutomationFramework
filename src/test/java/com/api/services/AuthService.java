b34d3beddf343a97e9c32ddfa8b2344726bc07e1
tulinag <debika0989@gmail.com>
2025-12-26 22:45:25 +0000

    Created the AuthService class to maintain the login API at one place

---
+++ src/test/java/com/api/services/AuthService.java
@@ -0,0 +1,26 @@
+package com.api.services;
+
+import static com.api.utils.SpecUtil.requestSpec;
+import static io.restassured.RestAssured.given;
+
+import java.io.IOException;
+
+import com.api.request.model.UserCredentials;
+
+import io.restassured.response.Response;
+
+public class AuthService {
+
+//service class!! It is going to hold the APIs that belongs to the Auth
+
+	private static final String LOGIN_ENDPOINT = "login";
+
+	public Response login(UserCredentials usercredentials) throws IOException {
+
+		Response response = given().spec(requestSpec(usercredentials)).when().post(LOGIN_ENDPOINT);
+
+		return response;
+
+	}
+
+}
